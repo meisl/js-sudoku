4187c89b672a7b5c38a30aada7c522728d35138b
meisl <m.kling@sprax.org>
2017-12-31 21:59:31 +0100

    - drop fn.extend - we'll go with class instead

--- scripts/fn.js
+++ scripts/fn.js
@@ -54,99 +54,12 @@ define([], () => {
 		return out;
 	};
 
-	function extend(...args) {
-		const tpThis = (this === null) ? "null" : typeof this;
-		if (tpThis !== "function")
-			throw new TypeError(
-				"function extend called on incompatible receiver: "
-				+ tpThis);
-		const thisProto = this.prototype;
-		const tpThisProto = typeof thisProto;
-		if (tpThisProto !== "object")
-			throw new TypeError(
-				"function extend called on incompatible receiver: "
-				+ "has invalid .prototype " + tpThisProto);
-		if (args.length === 0)
-			throw "missing ctor arg (expected a function)";
-		const [ctor, proto] = args;
-		const tpCtor = (ctor === null) ? "null" : typeof ctor;
-		if (tpCtor !== "function")
-			throw "invalid ctor arg; expected a function but got " + tpCtor;
-		if (thisProto === null) {
-			if (this.super !== undefined)
-				throw new TypeError(
-					"function extend called on incompatible receiver: "
-					+ ".super should be undefined but is " + this.super);
-		} else {
-			if (!thisProto.hasOwnProperty("constructor")) {
-				thisProto.constructor = this;
-			}
-			if (thisProto.constructor !== this) {
-				throw new TypeError(
-					"function extend called on incompatible receiver: "
-					+ ".prototype.constructor should be this "
-					+ "but is " + thisProto.constructor);
-			}
-			const thisSuper = Object.getPrototypeOf(thisProto).constructor;
-			if (!this.hasOwnProperty("super")) {
-				this.super = thisSuper;
-			}
-			if (this.super !== thisSuper) {
-				throw new TypeError(
-					"function extend called on incompatible receiver"
-					+ ": .super should be .prototype.__proto__.constructor"
-					+ " but is " + thisSuper);
-			}
-			if (thisProto.super === undefined) {
-				Reflect.defineProperty(thisProto, "super", {
-					get: function () {
-						const proto = Object.getPrototypeOf(this.constructor.prototype);
-						return (proto === null)
-							? undefined
-							: proto.constructor;
-					}
-				});
-			}
-		}
-		let result = ctor;
-		result.prototype = Object.create(thisProto, {
-			constructor: { value: result }
-		});
-		let tag;
-		if (ctor.name) {
-			tag = ctor.name;
-		} else if (thisProto !== null) {
-			tag = thisProto[Symbol.toStringTag];
-			if (tag)
-				tag = "Sub-" + tag;
-		} else if (this.name) {
-			tag = "Sub-" + this.name;
-		}
-		if (tag) {
-			Reflect.defineProperty(result.prototype, Symbol.toStringTag, {
-				value: tag
-			});
-		}
-		if (proto !== undefined) {
-			for (let key of Reflect.ownKeys(proto)) {
-				if (key === "constructor")
-					throw "invalid prop 'constructor' on proto template";
-				const desc = Reflect.getOwnPropertyDescriptor(proto, key);
-				Reflect.defineProperty(ctor.prototype, key, desc);
-			}
-		}
-		result.extend = this.extend;
-		result.super = this;
-		return result;
-	}
-
 	return Object.create(null, {
 		id: { value: id },
 		returnThis: { value: returnThis },
 		insist_nonNegativeInt: { value: insist_nonNegativeInt },
 		getDescriptors: { value: getDescriptors },
 		memoize: { value: memoize },
-		extend: { value: extend }
 	});
 });
 
