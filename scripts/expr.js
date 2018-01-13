c1940b52b8f94cae950ce2ffc79a45bea1968fff
meisl <m.kling@sprax.org>
2018-01-13 14:36:34 +0100

    start expr

---
+++ scripts/expr.js
@@ -0,0 +1,180 @@
+define(["./fn"], (fn) => { with (fn) {
+
+	class Expr {
+		static isExpr(x) {
+			return x instanceof Expr;
+		}
+		static parse(v) {
+			if (isExpr(v)) return v;
+			if (isString(v)) {
+				if (v === "")
+					throw "cannot parse empty string ''";
+				let i = v.indexOf(".");
+				if (i >= 0)
+					throw "NYI: accessor syntax in '" + v + "'";
+				return new VarExpr(v);
+			}
+			if (isArray(v)) {
+				if (v.length < 2)
+					throw "invalid app syntax [" + v + "] - need at least 2 elems";
+				return v.map(Expr.parse).reduce(Expr.app);
+			}
+			if (isFunction(v)) {
+				return new ConstExpr(v);
+			}
+			if (isObject(v)) {
+				throw "cannot parse object " + v;
+			}
+			return new ConstExpr(v);
+		}
+
+		static const(val) {
+			return new ConstExpr(val);
+		}
+		static var(name) {
+			return new VarExpr(name);
+		}
+		static app(f, arg) {
+			return new AppExpr(f, arg);
+		}
+		static if(cnd) {
+			const condX = Expr.parse(cnd);
+			return { then: function (thn) {
+				const thenX = Expr.parse(thn);
+				return {
+					else: function (els) {
+						const elseX = Expr.parse(els)
+						return new IfExpr(condX, thenX, elseX);
+					}
+				}
+			} };
+		}
+		
+		throwNotImplemented(which) {
+			throw which + " not implemented in " + this.constructor.name;
+		}
+
+		get isSelfEvaluating() {
+			this.throwNotImplemented("isSelfEvaluating");
+		}
+
+		get isConst()  { return false }
+		get isFun()    { return false }
+		get isVar()    { return false }
+		get isApp()    { return false }
+		get isIf()     { return false }
+	}
+	
+	class ConstExpr extends Expr {
+		constructor(value) {
+			super();
+			if (Number.isNaN(value))
+				throw "invalid arg NaN - cannot construct ConstExpr";
+			Object.assign(this, { value });
+		}
+		toString() {
+			const v = this.value;
+			if (fn.isString(v)) {
+				return "Const " + QUnit.dump.parse(v);
+			} else if (fn.isFunction(v)) {
+				if (v.name) {
+					return "Const " + v.name;
+				}
+				return "Const ?";
+			}
+			return "Const " + v;
+		}
+
+		eval(context) {
+			return this;
+		}
+		get isSelfEvaluating() { return true; }
+	}
+	Object.defineProperties(ConstExpr.prototype, {
+		[Symbol.toStringTag]: { value: "ConstExpr", configurable: true },
+		isConst: { value: true },
+	});
+
+	class VarExpr extends Expr {
+		static get nameRegExp() {
+			return /^[_a-zA-Z][_a-zA-Z0-9]*$/g;
+		}
+		static isValidName(x) {
+			return isString(x) && VarExpr.nameRegExp.test(x);
+		}
+		constructor(name) {
+			super();
+			if (!VarExpr.isValidName(name))
+				throw "invalid variable name "
+					+ (isString(name) ? "'" + name + "'" : name);
+			//this.name = name;
+			Object.assign(this, { name });
+		}
+		toString() {
+			return "Var " + QUnit.dump.parse(this.name);
+		}
+
+		get isSelfEvaluating() { return false; }
+		eval(context) {
+			const target = context[this.name];
+			if (!isExpr(target)) {
+				if (target === (void 0)) {
+					throw "unbound var " + name;
+				} else {
+					throw "bad context: var " + name + " bound to non-Expr " + target
+				}
+			}
+			return target;
+		}
+	}
+	Object.defineProperties(VarExpr.prototype, {
+		[Symbol.toStringTag]: { value: "VarExpr", configurable: true },
+		isVar: { value: true },
+	});
+
+	// Expr a = ...
+	//        | App (Expr a -> b) (Expr b)
+	class AppExpr extends Expr {
+		constructor(f, x) {
+			super();
+			if (!isExpr(f))
+				throw "invalid arg 'f' - not an Expr: " + f;
+			if (!isExpr(x))
+				throw "invalid arg 'x' - not an Expr: " + x;
+			Object.assign(this, { f, x });
+		}
+		toString() {
+			return "App (" + this.f.toString() + ") (" + this.x.toString() + ")";
+		}
+	}
+	Object.defineProperties(AppExpr.prototype, {
+		[Symbol.toStringTag]: { value: "AppExpr", configurable: true },
+		isApp: { value: true },
+	});
+
+	class IfExpr extends Expr {
+		constructor(condX, thenX, elseX) {
+			super();
+			Object.assign(this, { condX, thenX, elseX });
+		}
+		get isIf() { return true }
+
+		eval(context) {
+			const condE = this.condX.eval(context);
+			if (condE instanceof ConstExpr) {
+				return condE.value ? this.thenX.eval(context) : this.elseX.eval(context);
+			}
+			throw "evaluation error: " + condE;
+		}
+	}
+	Object.defineProperties(IfExpr.prototype, {
+		[Symbol.toStringTag]: { value: "IfExpr", configurable: true },
+		isIf: { value: true },
+	});
+
+	const isExpr = Expr.isExpr;
+
+
+
+	return Expr;
+} /* end with(fn) */ });
---
+++ test/08_expr-test.js
@@ -0,0 +1,334 @@
+require(["scripts/expr"], (Expr) => {
+	const { test, todo, skip, module } = QUnit;
+
+	const Const = Expr.const;
+	const Fun   = Expr.fun;
+	const Var   = Expr.var;
+	const App   = Expr.app;
+	const If    = Expr.if;
+
+	QUnit.assert.isExpr = function isExpr(value, desc) {
+		let message, result;
+		if (!desc) desc = "...";
+		message = ".isExpr(" + desc + ")"
+
+		result = value instanceof Expr;
+		if (result === false) {
+			this.pushResult({
+				result:   false,
+				actual:   value,
+				expected: "an Expr instance",
+				message:  message
+			});
+		} else {
+			result = Expr.isExpr(value);
+			if (result === true) {
+				this.pushResult({
+					result:   true,
+					actual:   value,
+					expected: "an Expr instance",
+					message:  message
+				});
+			} else {
+				this.pushResult({
+					result:   false,
+					actual:   value,
+					expected: "an Expr instance",
+					message:  message
+				});
+			}
+		}
+	};
+	QUnit.assert.isConst = function isConst(value, desc) {
+		this.isExpr(value, desc);
+		this.same(value.isConst, true,  desc + ".isConst");
+		this.same(value.isFun,   false, desc + ".isFun");
+		this.same(value.isVar,   false, desc + ".isVar");
+		this.same(value.isApp,   false, desc + ".isApp");
+		this.same(value.isIf,    false, desc + ".isIf");
+	};
+	QUnit.assert.isFun = function isFun(value, desc) {
+		this.isExpr(value, desc);
+		this.same(value.isConst, false, desc + ".isConst");
+		this.same(value.isFun,   true,  desc + ".isFun");
+		this.same(value.isVar,   false, desc + ".isVar");
+		this.same(value.isApp,   false, desc + ".isApp");
+		this.same(value.isIf,    false, desc + ".isIf");
+	};
+	QUnit.assert.isVar = function isConst(value, desc) {
+		this.isExpr(value, desc);
+		this.same(value.isConst, false, desc + ".isConst");
+		this.same(value.isFun,   false, desc + ".isFun");
+		this.same(value.isVar,   true,  desc + ".isVar");
+		this.same(value.isApp,   false, desc + ".isApp");
+		this.same(value.isIf,    false, desc + ".isIf");
+	};
+	QUnit.assert.isApp = function isConst(value, desc) {
+		this.isExpr(value, desc);
+		this.same(value.isConst, false, desc + ".isConst");
+		this.same(value.isFun,   false, desc + ".isFun");
+		this.same(value.isVar,   false, desc + ".isVar");
+		this.same(value.isApp,   true,  desc + ".isApp");
+		this.same(value.isIf,    false, desc + ".isIf");
+	};
+	QUnit.assert.isIf = function isConst(value, desc) {
+		this.isExpr(value, desc);
+		this.same(value.isConst, false, desc + ".isConst");
+		this.same(value.isFun,   false, desc + ".isFun");
+		this.same(value.isVar,   false, desc + ".isVar");
+		this.same(value.isApp,   false, desc + ".isApp");
+		this.same(value.isIf,    true,  desc + ".isIf");
+	};
+
+	module("Expr", () => { // ------------------------------------------
+		module(".var", () => { // ------------------------------------------
+			const validArgs = [
+				"_", "a", "b", "foo",
+				"__", "_0", "_bar",
+			];
+			const invalidArgs = [
+				true, false,
+				0, 1, -1, 42,
+				NaN, undefined,
+				null, {},
+				[],
+				() => 42,
+				//Symbol.iterator,	// TODO: does throw but only because Symbol cannot be converted to String
+				Expr.const(5),
+				"", " ", "-", "0", "1", "-a", 
+				"foo.bar", "foo-bar", "foo bar", 
+				"foo\tbar", "foo\n", "foo\rbar", "foo\r\nbar", "foo\n\rbar",
+				"()", "<>", "[]", "{}",
+			];
+			function runTests(assert, construct, desc) {
+				const mkDesc = v => desc + "(" + QUnit.dump.parse(v) + ")";
+				validArgs.forEach(v => {
+					const x    = construct(v);
+					const desc = mkDesc(v);
+					
+					assert.isVar(x, desc);
+					assert.same(x.name, v, desc + ".name");
+					assert.same(x.isSelfEvaluating, false, desc + ".isSelfEvaluating");
+				});
+				invalidArgs.forEach(v => {
+					const desc = mkDesc(v);
+					assert.throws(() => construct(v), /invalid/, desc + " should throw");
+				});
+			}
+			test("Expr.var", function (assert) {
+				runTests(assert, v => Expr.var(v), "Expr.var");
+			});
+			test(".parse(...)", function (assert) {
+				let x, desc;
+
+				x = Expr.parse("x");
+				desc = "parse('x')";
+				assert.isVar(x, desc);
+				assert.same(x.name, "x", desc + ".name");
+			});
+			test(".toString", function (assert) {
+				validArgs.forEach(name => {
+					const act  = Var(name).toString();
+					const desc = "Var('" + name + "').toString()";
+					const exp  = 'Var "' + name + '"';
+					assert.same(act, exp, desc);
+				});
+			})
+		}); // end module ".var"
+
+		module(".const", () => { // ------------------------------------------
+			const validArgs = [
+				true, false,
+				"", "foo", 
+				0, 1, -1, 42,
+				null, undefined, {},
+				Symbol.iterator,
+				x => x + 1,
+			];
+			const invalidArgs = [NaN];
+			function runTests(assert, construct, desc) {
+				const mkDesc = v => desc + "(" + QUnit.dump.parse(v) + ")";
+				validArgs.forEach(v => {
+					const x    = construct(v);
+					const desc = mkDesc(v);
+					assert.isConst(x, desc);
+					assert.same(x.value, v, desc + ".value");
+					assert.same(x.isSelfEvaluating, true, desc + ".isSelfEvaluating");
+				});
+				invalidArgs.forEach(v => {
+					const desc = mkDesc(v);
+					assert.throws(() => construct(v), /invalid/, desc + " should throw");
+				});
+			}
+			test("create with Expr.const(...)", function (assert) {
+				runTests(assert, v => Expr.const(v), "Expr.const");
+			});
+
+			const bools = [true, false];
+			const numbers = [-1, 0, 1, 42, 3.1415];
+			const strings = ["", "foo", "\"bar\"", "a b c"];
+				
+			test(".parse(...)", function (assert) {
+				bools.forEach(v => {
+					assert.deepEqual(
+						Expr.parse(v),
+						Const(v),
+						"Expr.parse(" + v + ")"
+					)
+				});
+				numbers.forEach(v => {
+					assert.deepEqual(
+						Expr.parse(v),
+						Const(v),
+						"Expr.parse(" + v + ")"
+					)
+				});
+			});
+			module(".toString", () => { // ------------------------------------------
+				test("bools", function (assert) {
+					bools.forEach(v => {
+						const act  = Const(v).toString();
+						const desc = "Const(" + v + ").toString()";
+						const exp  = "Const " + v;
+						assert.same(act, exp, desc);
+					});
+				});
+				test("numbers", function (assert) {
+					numbers.forEach(v => {
+						const act  = Const(v).toString();
+						const desc = "Const(" + v + ").toString()";
+						const exp  = "Const " + v;
+						assert.same(act, exp, desc);
+					});
+				});
+				test("strings", function (assert) {
+					strings.forEach(v => {
+						const act  = Const(v).toString();
+						const desc = "Const('" + v + "').toString()";
+						const exp  = "Const " + QUnit.dump.parse(v);
+						assert.same(act, exp, desc);
+					});
+				});
+			}) // end module ".toString"
+		}); // end module ".const"
+
+		module(".app", () => { // ------------------------------------------
+			test("Expr.app", function (assert) {
+				let f, a, x, desc;
+
+				f = Expr.var("f");
+				a = Expr.const(42);
+				x = Expr.app(f, a);
+				desc = "(App (Var 'f') (Const 42))"
+
+				assert.isApp(x, desc);
+				assert.same(x.f, f, desc + ".f");
+				assert.same(x.x, a, desc + ".x");
+			});
+			module(".parse", () => { // ------------------------------------------
+				test("[Var,Const]", function (assert) {
+					assert.deepEqual(
+						Expr.parse(["f", 42]),
+						App(Var("f"), Const(42)),
+						"parse['f', 42]"
+					);
+				});			
+				test("[Var,Const,Const]", function (assert) {
+					assert.deepEqual(
+						Expr.parse(["f", 42, 5]),
+						App(App(Var("f"), Const(42)), Const(5)),
+						"parse['f', 42, 5]"
+					);
+				});
+				test("[Var,[Var,Const]]", function (assert) {
+					assert.deepEqual(
+						Expr.parse(["f", ["g", 5]]),
+						App(Var("f"), App(Var("g"), Const(5))),
+						"parse['f', ['g', 5]]"
+					);
+				});
+				test("[Expr,Const]", function (assert) {
+					function aFunction(x) {
+						return x;
+					}
+					assert.deepEqual(
+						Expr.parse([Const(aFunction), 42]),
+						App(Const(aFunction), Const(42)),
+						"parse[Expr.const(aFunction), 42]"
+					);
+				});
+				test("[Fn,Const]", function (assert) {
+					function aFunction(x) {
+						return x;
+					}
+					assert.deepEqual(
+						Expr.parse([aFunction, 42]),
+						App(Const(aFunction), Const(42)),
+						"parse[aFunction, 42]"
+					);
+				});
+			}); // end module ".parse"
+			
+			module(".toString", () => { // ------------------------------------------
+				test("[Var,Const]", function (assert) {
+					const x   = Expr.parse(["f", 42]);
+					//const x   = App(Var("f"), Const(42));
+					const exp = 'App (Var "f") (Const 42)';
+					assert.same(x.toString(), exp);
+				});
+				test("[Var,Const,Const]", function (assert) {
+					const x   = Expr.parse(["f", 42, 5]);
+					//const x   = App(App(Var("f"), Const(42)), Const(5));
+					const exp = 'App (App (Var "f") (Const 42)) (Const 5)';
+					assert.same(x.toString(), exp);
+				});
+				test("[Var,[Var,Const]]", function (assert) {
+					const x   = Expr.parse(["f", ["g", 5]]);
+					//const x   = App(Var("f"), App(Var("g"), Const(5)));
+					const exp = 'App (Var "f") (App (Var "g") (Const 5))';
+					assert.same(x.toString(), exp);
+				});
+				test("[Fn,Const]", function (assert) {
+					function namedFn(x) {
+						return x;
+					}
+					const anonFn = function (x) {
+						return x;
+					};
+					const arrowFn = x => x;
+					let x, exp;
+					
+					x   = Expr.parse([namedFn, 42]);
+					exp = "App (Const namedFn) (Const 42)";
+					assert.same(x.toString(), exp);
+					
+					x   = Expr.parse([anonFn, 42]);
+					exp = "App (Const ?) (Const 42)";
+					assert.same(x.toString(), exp);
+					
+					x   = Expr.parse([arrowFn, 42]);
+					exp = "App (Const ?) (Const 42)";
+					assert.same(x.toString(), exp);
+				});
+			}); // end module ".toString"
+		}); // end module ".app"
+
+		module(".if", () => { // ------------------------------------------
+			test("Expr.if", function (assert) {
+				let c, t, e, x, desc;
+
+				c = Expr.var("cond");
+				t = Expr.const("then");
+				e = Expr.const("else");
+				x = Expr.if(c).then(t).else(e);
+				desc = "(If (Var 'cond') (Const 'then') (Const 'else'))"
+
+				assert.isIf(x, desc);
+				assert.same(x.condX, c, desc + ".condX");
+				assert.same(x.thenX, t, desc + ".thenX");
+				assert.same(x.elseX, e, desc + ".elseX");
+			});
+		}); // end module ".app"
+
+	}); // end module "Expr"
+}); // end require
